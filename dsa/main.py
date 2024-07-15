06ca4e21abf56c44383f3cdc538faed06bbc9a4b
ahmed <ahmedlearn01@gmail.com>
2024-07-15 11:14:47 +0300

    binary search

--- dsa/main.py
+++ dsa/main.py
@@ -1,7 +1,7 @@
 # Linear search
 def linear_search(list, target):
-    for i in range(len(list)):
-        if i == target:
+    for i in range(0, len(list)):
+        if list[i] == target:
             return i
     return None
 
@@ -14,3 +14,23 @@ def varify(result):
 numbers = [1,2,3,4,5,6,7,8,9,10]
 result = linear_search(numbers, 8)
 varify(result)
+
+# Binary Search
+def binary_search(list, target):
+    first = 0
+    last = len(list) - 1
+
+    while first <= last:
+        middpoint = (first + last) // 2
+
+        if list[middpoint] == target:
+            return middpoint
+        elif list[middpoint] < target:
+            first = middpoint + 1
+        else: 
+            last = middpoint - 1
+    return None
+
+
+res = binary_search(numbers, 8)
+varify(res)
