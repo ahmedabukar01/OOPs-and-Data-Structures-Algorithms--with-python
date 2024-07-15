4770c0e222b36e94fea94f40f906d2e57f0fb9b4
ahmed <ahmedlearn01@gmail.com>
2024-07-15 14:17:56 +0300

    recursive binary search

--- dsa/main.py
+++ dsa/main.py
@@ -34,3 +34,21 @@ def binary_search(list, target):
 
 res = binary_search(numbers, 8)
 varify(res)
+
+# Recursive binary search
+def recursive_binary_search(list, target):
+    if len(list) == 0:
+        return False
+    else:
+        midpoint = (len(list))//2
+        if list[midpoint] == target:
+            return True
+        else:
+            if list[midpoint] < target:
+                return recursive_binary_search(list[midpoint +1:], target)
+            else: 
+                return recursive_binary_search(list[:midpoint], target)
+            
+
+ress = recursive_binary_search(numbers, 8)
+varify(ress)
