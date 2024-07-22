c9693e41b24f246bb792f9a94eaf5f1c682a1ce7
ahmed <ahmedlearn01@gmail.com>
2024-07-22 11:36:06 +0300

    repr linked List & search

--- dsa/main.py
+++ dsa/main.py
@@ -91,3 +91,31 @@ class Linked_list:
         new_node = Node(data)
         new_node.next_node = self.head
         self.head = new_node
+    
+    def search(self, key):
+        current = self.head
+
+        while current:
+            if current.data == key:
+                return current
+            else:
+                current = current.next_node
+        return None
+
+    # display format on the console
+    def __repr__(self):
+        # return string representation of the list
+
+        nodes = []
+        current = self.head
+
+        while current:
+            if current is self.head:
+                nodes.append("Head: %s" % current.data)
+            elif current.next_node is None:
+                nodes.append(f"Tail {current.data}")
+            else: 
+                nodes.append("%s" % current.data)
+
+            current = current.next_node
+        return '-> '.join(nodes)
