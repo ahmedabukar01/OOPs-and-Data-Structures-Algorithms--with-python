6c45c15b71dc17c3c054e32d872c9db23bd0af94
ahmed <ahmedlearn01@gmail.com>
2024-07-25 16:37:30 +0300

    linked list merge process

--- dsa/main.py
+++ dsa/main.py
@@ -279,7 +279,50 @@ def split_linked(linked_list):
 
 def merge_linked_sort(left,right):
     """
-    Merges two linked lists, sorting by data in nodes
+    Merges two linked lists, sorting by data in nodes.
+    Returns a new merged lists
     """
-    pass
-    
+
+    merged = Linked_list()
+    # Add fake head that is discarded later
+    merged.add(0)
+
+    current = merged.head
+
+    left_head = left.head
+    right_head = right.head
+
+    # Iterate over left and right until we reach the tail node of either.
+    while left_head or right_head:
+        if left_head is None:
+            current.next_node = right_head
+            right_head = right_head.next_node
+        # If the head node of right is None, we're past the tail 
+        # Add the tail node from left to merged linked list
+        elif right_head is None:
+            current.next_node = left_head
+            # call next on left to set loop condition on false
+            left_head = left_head.next_node
+        else: 
+            # Not at either tail node
+            # Obtain node data to perform comparison operations
+            left_data = left_head.data
+            right_data = right_head.data
+            # if data on left is less than right, set current to left node
+            if left_data < right_data: 
+                current.next_node = left_head
+                # Move left head to next node
+                left_head = left_head.next_node
+            # If data on left is greater than right, set current to right node
+            else: 
+                current.next_node = right_head
+                # Move right head to next node 
+                right_head = right_head.next_node
+            # Move current to next node
+            current = current.next_node
+
+            # Discard fake head and set first merged node as head
+            head = merged.head.next_node
+            merged.head = head
+
+            return merged
