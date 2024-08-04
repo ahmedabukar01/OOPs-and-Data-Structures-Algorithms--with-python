f2b7f578bc3b3adbe40c11553449e51ac6518178
ahmed <ahmedlearn01@gmail.com>
2024-08-04 10:45:05 +0300

    quick sort

--- dsa/main.py
+++ dsa/main.py
@@ -386,8 +386,25 @@ print(selection_sort(s_values))
 def sum(numbers):
     if not numbers:
         return 0
-    print("calling %s", numbers[1:])
+    print("calling %d", numbers[1:])
     remainingSum = sum(numbers[1:])
     print(f" {remainingSum} this is numbers: {numbers} ")
     return numbers[0] + remainingSum
 print(sum([3,65,6,4,2]))
+
+# quicksort
+def quicksort(values):
+    if len(values) <= 1:
+        return values
+    less_than_pivot = []
+    greater_than_pivot = []
+    pivot = values[0]
+
+    for value in values[1:]:
+        if value <= pivot:
+            less_than_pivot.append(value)
+        else:
+            greater_than_pivot.append(value)
+    return quicksort(less_than_pivot) + [pivot] + quicksort(greater_than_pivot)
+sorted_values = quicksort([89,3,5,34,90,2])
+print(sorted_values)
